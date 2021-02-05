1b320194dcbf8c9af136fbe70bb437a9dd977f78
Niko Tapani Tauriainen <niko.tauriainen@hotmail.com>
2021-02-05 12:36:12 +0200

    added TTS and STT functions

--- app/src/main/java/com/example/myapplication/NotepadActivity.java
+++ app/src/main/java/com/example/myapplication/NotepadActivity.java
@@ -1,15 +1,28 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -19,17 +32,24 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import static com.google.android.gms.common.internal.safeparcel.SafeParcelable.NULL;
 
 public class NotepadActivity extends AppCompatActivity {
 
+    //Define audioRequestCode
+    public static final Integer RecordAudioRequestCode = 1;
+
     //TAG for error logs
     private static final String TAG = "FROM NOTEPAD ACTIVITY";
 
     //Initializing UI and variables
+    private SpeechRecognizer speechRecognizer;
+    private ImageView ui_micButton;
     private EditText ui_text;
     private Button ui_button;
     private Button ui_openButton;
@@ -52,18 +72,81 @@ public class NotepadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notepad);
 
-        //UI elements
-        ui_button = (Button)findViewById(R.id.bSave);
-        ui_openButton = (Button)findViewById(R.id.bOpen);
-        ui_deleteButton = (Button)findViewById(R.id.bDelete);
-
         //get user credentials from shared preferences, to use username in database queries
         SharedPreferences sp1 = getSharedPreferences("Credentials",MODE_PRIVATE);
         userString = sp1.getString("username",null);
 
+        //UI elements
+        ui_button = (Button)findViewById(R.id.bSave);
+        ui_openButton = (Button)findViewById(R.id.bOpen);
+        ui_deleteButton = (Button)findViewById(R.id.bDelete);
+        ui_micButton = (ImageView)findViewById(R.id.micButton);
+        ui_text = (EditText) findViewById(R.id.etWrite);
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        //STT init (recognize speech and insert into ui_text)
+        final Intent sttIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                ui_text.setText("");
+                ui_text.setHint("Listening...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                ui_text.setText(data.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
         //Check if activity started with intent extra (opened existing note),
         //if yes set url as update type, and if no set oldString as something as volley cant handle null parameters
-        ui_text = (EditText) findViewById(R.id.etWrite);
         oldString = getIntent().getStringExtra("EXTRA_NOTE");
         if(oldString == null){
             oldString = "filling";
@@ -75,6 +158,7 @@ public class NotepadActivity extends AppCompatActivity {
             urlInsert = urlUpdate;
             ui_text.setText(oldString);
         }
+
 
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -166,5 +250,23 @@ public class NotepadActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        ui_micButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    speechRecognizer.startListening(sttIntent);
+                }
+                return false;
+            }
+        });
     }
+
 }
